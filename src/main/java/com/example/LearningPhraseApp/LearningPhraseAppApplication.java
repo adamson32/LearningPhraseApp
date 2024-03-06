ed3542239e0a2daf00283d7b1b480f11c51c0f78
Adam Dziakiewicz <adam.dziakiewicz@outlook.com>
2024-03-06 14:52:28 +0100

    Create empty project

---
+++ src/main/java/com/example/LearningPhraseApp/LearningPhraseAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearningPhraseApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningPhraseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningPhraseAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LearningPhraseApp/LearningPhraseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LearningPhraseApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningPhraseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
